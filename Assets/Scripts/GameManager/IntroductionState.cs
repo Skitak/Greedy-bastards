2e3422d33d508b2a91d2e6b59516d04a63aa90c5
skitak <bas2205@live.fr>
2019-01-27 12:35:36 +0100

    intro scene

--- Assets/Scripts/GameManager/IntroductionState.cs
+++ Assets/Scripts/GameManager/IntroductionState.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class IntroductionState : GameState {
-    
-    string introScene = "Introduction Scene";
-    string gameScene = "Game Scene";
     public override void Enter(){
-        SceneManager.LoadScene(introScene);
+        SceneManager.LoadScene(1);
     }
     public override void UpdateState(float delta){
         if (Input.anyKey)
             GameManager.ChangeState(new PlayerJoinState());
     }
     public override void Exit(){
-        SceneManager.LoadScene(gameScene);
+        SceneManager.LoadScene(0);
+    }
+
+    public void Play() {
+        GameManager.ChangeState(GameManager.instance.states.playerJoinState);
     }
 }
