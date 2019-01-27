63bfd2356b6f4fc0b5975a979b8d5f52be68532e
skitak <bas2205@live.fr>
2019-01-27 10:46:03 +0100

    lootbag icon added

---
+++ Assets/Scripts/LootBagIcon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LootBagIcon : MonoBehaviour
+{
+    Transform followThis;
+    Vector2 offset = new Vector2(0, -100);
+    Image image;
+    public void SetFollow(Transform followThis){
+        this.followThis = followThis;
+    }
+
+    private void Start() {
+        image = GetComponent<Image>();
+    }
+
+    private void Update() {
+        if (followThis == null) {
+            image.enabled = false;
+            return;
+        }
+ 
+        image.enabled = true;
+        Vector2 sp = Camera.main.WorldToScreenPoint(followThis.position);
+ 
+        this.transform.position = sp + offset;
+    }
+
+
+}
